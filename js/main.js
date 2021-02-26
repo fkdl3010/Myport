107c7615b1757955f094e40a77bc8dd5fcbacf11
fkdl3010 <fkdl3919@icloud.com>
2021-02-26 11:08:35 +0900

    css 수정 및 fullPage 적용

--- js/main.js
+++ js/main.js
@@ -14,21 +14,21 @@
 		offset: $(window).height() / 2
 	});
 
-	///////////////////////////
+	/////////////////////////
 	// Smooth scroll
-	$("#nav .main-nav a[href^='#']").on('click', function(e) {
-		e.preventDefault();
-		var hash = this.hash;
-		$('html, body').animate({
-			scrollTop: $(this.hash).offset().top
-		}, 600);
-	});
+	// $("#nav .main-nav a[href^='#']").on('click', function(e) {
+	// 	e.preventDefault();
+	// 	var hash = this.hash;
+	// 	$('html, body').animate({
+	// 		scrollTop: $(this.hash).offset().top
+	// 	}, 600);
+	// });
 
-	$('#back-to-top').on('click', function(){
-		$('body,html').animate({
-			scrollTop: 0
-		}, 600);
-	});
+	// $('#back-to-top').on('click', function(){
+	// 	$('body,html').animate({
+	// 		scrollTop: 0
+	// 	}, 600);
+	// });
 
 	///////////////////////////
 	// Btn nav collapse
